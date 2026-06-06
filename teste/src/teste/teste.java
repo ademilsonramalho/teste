8834b33a4ba6ee418c4a51703c2ae6ef84a92b54
ademilsonramalho <ramalhosanto@gmail.com>
2026-06-06 17:12:58 -0300

    Commit projeto completo inicial para Git Hub

---
+++ teste/src/teste/teste.java
@@ -0,0 +1,12 @@
+package teste;
+
+//import jdk.internal.org.jline.terminal.TerminalBuilder.SystemOutput;
+
+public class teste {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Alo motooooooooooooo");
+	}
+
+}
